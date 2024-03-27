69ee27e5e1c6312f22a361378e25b2d7532a3213
maurom23 <maurosantosfelipe23@gmail.com>
2024-03-26 22:40:07 -0300

    mauroteste

--- recebeDados.test.js
+++ recebeDados.test.js
@@ -2,9 +2,9 @@
 const dadosEstudante = require('./dadosEstudante');
 
 describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+  test('Verifica se a função processa corretamente o nome "Mauro Felipe" e o RA "063674"', () => {
+    const nome = "Mauro Felipe";
+    const RA = "063674";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
